19f057dd7b3ecf006d04ce3b67ca3217fe0a2464
Erika Korkute <erikakorkute@gmail.com>
2025-06-06 14:36:55 +0200

    Actualización 6/6/25.

--- src/main/java/org/example/demo/App.java
+++ src/main/java/org/example/demo/App.java
@@ -27,7 +27,6 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
 // Metodo principal de la aplicación.
     public static void main(String[] args) {
         launch();
